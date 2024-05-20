17205c3949e014592dc52206ab06feb6271bd2c0
Edwin Harmata <edwin.harmata@gmail.com>
2024-05-20 13:45:37 +0200

    Project initialization

---
+++ template-engine/src/test/java/com/cvgenerator/templateengine/TemplateEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvgenerator.templateengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
